335dd85c223299356708beeced22f31cf0079204
AlexandraTchalakian <81053124+AlexandraTchalakian@users.noreply.github.com>
2021-03-28 16:03:45 +0200

    Update AnalysePendulum5.m

--- Chaos/AnalysePendulum5.m
+++ Chaos/AnalysePendulum5.m
@@ -1,18 +1,9 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Scan on parameters from Matlab
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Ce script Matlab decrit une facon d'automatiser la production 
-% de resultats, lorsqu'on doit faire une serie de simulations 
-% en variant un des parametres d'entree.
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Initialize
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Define some variables
 
-% MODIFY this according to your file system setup
 workingfolder = './'; % Path to the folder that contains the binary 
 % of the code and the simulations (must end with /). Here we execute the
 % script in the same folder.
@@ -21,7 +12,6 @@ ndigit = 8; % Precision used in numerical to string conversion for input data fi
 solver='StormerVerlet';
 
 % Define parameters: scanned and constant ones
-% MODIFY this according to your needs.
 % NB: scanned parameter must always be the first in these lines.
 dt = [0.001,0.003,0.005,0.008,0.1]; %here, scan on dt, other parameters are kept constant.
 nsimul = length( dt ); %number of points in the scan.
@@ -46,8 +36,6 @@ nparams = length(inputparam_string);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Simulations
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%NB: DO NOT MODIFY THIS (unless you really want to...)
 
 %Loop on the scanned parameter
 %For each value, create an input file,
@@ -100,8 +88,6 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Simulation analysis
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%MODIFY this according to your needs.
 
 %for each file, load the data and, as an example, plot all the simulations
 %on the same plot
